90ed6986638a13adeb9014a30b0d3cf34f13199f
Hiro Mia <hiromia006@gmail.com>
2022-11-30 22:46:50 +0600

    Batch Three Rest Assured

--- src/test/java/com/sqlLearningAcademy/restAssured/batch3/tests/Students.java
+++ src/test/java/com/sqlLearningAcademy/restAssured/batch3/tests/Students.java
@@ -1,9 +1,14 @@
 package com.sqlLearningAcademy.restAssured.batch3.tests;
 
+import com.thedeanda.lorem.LoremIpsum;
 import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Students extends BaseTest {
 
@@ -39,5 +44,213 @@ public class Students extends BaseTest {
                 .statusCode(200)
                 .log().body();
 
+    }
+
+    @Test
+    public void createStudentHashMapShouldSucceed() {
+        HashMap<String, Object> studentJson = new HashMap<>();
+        studentJson.put("batchId", 1);
+        studentJson.put("name", LoremIpsum.getInstance().getName());
+        studentJson.put("contactNo", LoremIpsum.getInstance().getPhone());
+
+        System.out.println(studentJson);
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("Content-Type", "application/json")
+                .body(studentJson.toString())
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/students")
+                .then()
+                .statusCode(200)
+                .log().body();
+    }
+
+    @Test
+    public void createStudentJsonShouldSucceed() {
+        JSONObject studentJson = new JSONObject();
+        studentJson.put("batchId", 1);
+        studentJson.put("name", LoremIpsum.getInstance().getName());
+        studentJson.put("contactNo", LoremIpsum.getInstance().getPhone());
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("Content-Type", "application/json")
+                .body(studentJson.toString())
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/students")
+                .then()
+                .statusCode(201)
+                .log().body();
+    }
+
+    @Test
+    public void createStudentJsonAssertionShouldSucceed() {
+        String name = LoremIpsum.getInstance().getName();
+        String contactNo = LoremIpsum.getInstance().getPhone();
+
+        JSONObject studentJson = new JSONObject();
+        studentJson.put("batchId", 1);
+        studentJson.put("name", name);
+        studentJson.put("contactNo", contactNo);
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("Content-Type", "application/json")
+                .body(studentJson.toString())
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/students")
+                .then()
+                .log().body()
+                .statusCode(201)
+                .body("name", equalTo(name))
+                .body("contactNo", equalTo(contactNo))
+                .body("batchId", equalTo(1));
+    }
+
+    @Test
+    public void createStudentIndepenedShouldSucceed() {
+        int batchId = given()
+                .contentType(ContentType.JSON)
+                .log().uri()
+                .when()
+                .get("/batch")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .extract().jsonPath().getInt("[0].id");
+
+
+        String name = LoremIpsum.getInstance().getName();
+        String contactNo = LoremIpsum.getInstance().getPhone();
+
+        JSONObject studentJson = new JSONObject();
+        studentJson.put("batchId", batchId);
+        studentJson.put("name", name);
+        studentJson.put("contactNo", contactNo);
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("Content-Type", "application/json")
+                .body(studentJson.toString())
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/students")
+                .then()
+                .log().body()
+                .statusCode(201)
+                .body("name", equalTo(name))
+                .body("contactNo", equalTo(contactNo))
+                .body("batchId", equalTo(batchId));
+    }
+
+    @Test
+    public void updateStudentShouldSucceed() {
+        int batchId = given()
+                .contentType(ContentType.JSON)
+                .log().uri()
+                .when()
+                .get("/batch")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .extract().jsonPath().getInt("[0].id");
+
+        String name = LoremIpsum.getInstance().getName();
+        String contactNo = LoremIpsum.getInstance().getPhone();
+        JSONObject studentJson = new JSONObject();
+        studentJson.put("batchId", batchId);
+        studentJson.put("name", name);
+        studentJson.put("contactNo", contactNo);
+
+        int studentId = given()
+                .contentType(ContentType.JSON)
+                .header("Content-Type", "application/json")
+                .body(studentJson.toString())
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/students")
+                .then()
+                .log().body()
+                .statusCode(201)
+                .body("name", equalTo(name))
+                .body("contactNo", equalTo(contactNo))
+                .body("batchId", equalTo(batchId))
+                .extract().jsonPath().getInt("id");
+
+        String updateName = LoremIpsum.getInstance().getName();
+        String updateContactNo = LoremIpsum.getInstance().getPhone();
+        JSONObject updateStudent = new JSONObject();
+        studentJson.put("batchId", batchId);
+        studentJson.put("name", updateContactNo);
+        studentJson.put("contactNo", updateName);
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("Content-Type", "application/json")
+                .body(updateStudent.toString())
+                .log().uri()
+                .log().body()
+                .when()
+                .put("/students/" + studentId)
+                .then()
+                .log().body()
+                .statusCode(200);
+
+    }
+
+    @Test
+    public void deleteStudentShouldSucceed() {
+        int batchId = given()
+                .contentType(ContentType.JSON)
+                .log().uri()
+                .when()
+                .get("/batch")
+                .then()
+                .statusCode(200)
+                .log().body()
+                .extract().jsonPath().getInt("[0].id");
+
+        String name = LoremIpsum.getInstance().getName();
+        String contactNo = LoremIpsum.getInstance().getPhone();
+        JSONObject studentJson = new JSONObject();
+        studentJson.put("batchId", batchId);
+        studentJson.put("name", name);
+        studentJson.put("contactNo", contactNo);
+
+        int studentId = given()
+                .contentType(ContentType.JSON)
+                .header("Content-Type", "application/json")
+                .body(studentJson.toString())
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/students")
+                .then()
+                .log().body()
+                .statusCode(201)
+                .body("name", equalTo(name))
+                .body("contactNo", equalTo(contactNo))
+                .body("batchId", equalTo(batchId))
+                .extract().jsonPath().getInt("id");
+
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("Content-Type", "application/json")
+                .log().uri()
+                .log().body()
+                .when()
+                .delete("/students/" + studentId)
+                .then()
+                .log().body();
     }
 }
