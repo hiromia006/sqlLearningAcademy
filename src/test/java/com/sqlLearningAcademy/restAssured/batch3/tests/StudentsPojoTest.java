05586ebfa2d649acb61d081aeb7dea55dd241e2c
Hiro Mia <hiromia006@gmail.com>
2022-12-03 22:38:19 +0600

    Batch Three Rest Assured

---
+++ src/test/java/com/sqlLearningAcademy/restAssured/batch3/tests/StudentsPojoTest.java
@@ -0,0 +1,42 @@
+package com.sqlLearningAcademy.restAssured.batch3.tests;
+
+import com.sqlLearningAcademy.restAssured.batch3.tests.pojo.Student;
+import com.thedeanda.lorem.LoremIpsum;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class StudentsPojoTest extends BaseTest {
+    @Test
+    public void createStudentShouldSucceed() {
+        Student student = new Student(1, LoremIpsum.getInstance().getName(), LoremIpsum.getInstance().getPhone());
+        given()
+                .body(student)
+                .log().uri()
+                .log().body()
+                .when()
+                .post("/students")
+                .then()
+                .statusCode(201)
+                .log().body();
+    }
+
+
+    @Test
+    public void getStudentShouldSucceed() {
+        List<Student> students = given()
+                .log().uri()
+                .when()
+                .get("/students")
+                .then()
+                .statusCode(200)
+
+                .extract().jsonPath().getList("", Student.class);
+
+        for (Student student : students) {
+            System.out.println("Student name: " + student.getName() + " Phone " + student.getContactNo());
+        }
+    }
+}
--- src/test/java/com/sqlLearningAcademy/restAssured/batch3/tests/pojo/Student.java
+++ src/test/java/com/sqlLearningAcademy/restAssured/batch3/tests/pojo/Student.java
@@ -6,15 +6,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Student {
-
     @JsonProperty("name")
-    public String name;
+    private String name;
 
     @JsonProperty("batchId")
-    public Integer batchId;
+    private Integer batchId;
 
     @JsonProperty("contactNo")
-    public String contactNo;
+    private String contactNo;
+
+    @JsonProperty("id")
+    private Integer id;
 
 
     /**
@@ -28,11 +30,34 @@ public class Student {
      * @param batchId
      * @param contactNo
      */
-    public Student(String name, Integer batchId, String contactNo) {
-        super();
+    public Student(Integer batchId, String name, String contactNo) {
         this.name = name;
         this.batchId = batchId;
         this.contactNo = contactNo;
+    }
+
+
+    public Student(Integer batchId, String name, String contactNo, Integer id) {
+        this.batchId = batchId;
+        this.name = name;
+        this.contactNo = contactNo;
+        this.id=id;
+    }
+
+    public Integer getBatchId() {
+        return batchId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getContactNo() {
+        return contactNo;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
 }
